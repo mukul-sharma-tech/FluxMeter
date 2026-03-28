23336a5c9b67930dec87435ce575120a681876b7
Mukul Sharma <muku0784@gmail.com>
2026-03-28 22:21:10 +0530

    ts fix

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,8 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactCompiler: true,
-// };
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  experimental: {
-    // This line tells Next.js to not bundle esbuild
-    // and treat it as an external package on the server.
-    // This is required for your Inngest API route to work.
-    serverComponentsExternalPackages: ["esbuild"],
-  },
+  serverExternalPackages: ["esbuild"],
 };
 
 export default nextConfig;
